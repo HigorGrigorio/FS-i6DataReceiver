dda9d0f7c973bc6e39c1d63e70feeffa48989b27
Higor Grigorio <higorgrigorio@gmail.com>
2022-07-25 15:28:54 -0300

    fixed the switch port

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <PWM.h>
 
-PWM channels[4];
+PWM channels[5];
 
 void setup()
 {
@@ -11,17 +11,19 @@ void setup()
     channels[1].setPin(4);
     channels[2].setPin(7);
     channels[3].setPin(8);
+    channels[4].setPin(9);
 
     channels[0].calculateOffSet();
     channels[1].calculateOffSet();
     channels[2].calculateOffSet();
     channels[3].calculateOffSet();
-
+    channels[4].calculateOffSet();
 
     Serial.println(String("channel {1} offset: ") + String(channels[0].getOffset()));
     Serial.println(String("channel {2} offset: ") + String(channels[1].getOffset()));
     Serial.println(String("channel {3} offset: ") + String(channels[2].getOffset()));
     Serial.println(String("channel {4} offset: ") + String(channels[3].getOffset()));
+    Serial.println(String("channel {4} offset: ") + String(channels[4].getOffset()));
 
     delay(2000);
 }
@@ -31,6 +33,7 @@ void loop()
     Serial.print(String("channel 1: ") + String(channels[0].read()) + "   ");
     Serial.print(String("channel 2: ") + String(channels[1].read()) + "   ");
     Serial.print(String("channel 3: ") + String(channels[2].read()) + "   ");
-    Serial.println(String("channel 4: ") + String(channels[3].read()));
+    Serial.print(String("channel 4: ") + String(channels[3].read()) + "   ");
+    Serial.println(String("channel 5: ") + String(channels[4].read()));
     Serial.flush();
 }
